062dd4fc8316a2521c224d4a418f7933f7bf8201
mrosa16 <michael.rosa@holu.com.br>
2024-04-06 18:13:30 -0300

    feat: insert validation error in errror!

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import { BasRequestException } from "@exceptions/bad-request-exception";
 import { PrismaClient } from "@prisma/client";
 import { notFoundException } from "../../exceptions/not-found-exceptions";
 import { userInsertDTO } from "./dtos/user-insert.dto";
@@ -13,7 +14,43 @@ export const getUsers = async (): Promise<userModel[]> => {
   return users;
 };
 
+export const getUserByCpf = async (cpf: string): Promise<userModel> => {
+  const user = await prisma.user.findFirst({
+    where: { cpf },
+  });
+
+  if (!user) {
+    throw new notFoundException("User");
+  }
+
+  return user;
+};
+
+export const getUserByEmail = async (email: string): Promise<userModel> => {
+  const user = await prisma.user.findFirst({
+    where: { email },
+  });
+
+  if (!user) {
+    throw new notFoundException("User");
+  }
+
+  return user;
+};
+
 export const createUser = async (body: userInsertDTO): Promise<userModel> => {
+  const userEmail = await getUserByEmail(body.email).catch(() => undefined);
+
+  if (userEmail) {
+    throw new BasRequestException("Email exist in DB");
+  }
+
+  const userCpf = await getUserByCpf(body.cpf).catch(() => undefined);
+
+  if (userCpf) {
+    throw new BasRequestException("CPF exist in DB");
+  }
+
   return prisma.user.create({
     data: body,
   });
