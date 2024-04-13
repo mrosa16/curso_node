dea2a9c251b628185ef6088382ab79d8deb4a43c
mrosa16 <michael.rosa@holu.com.br>
2024-04-12 21:44:53 -0300

    feat> validat password

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,16 @@
+import { notFoundException } from "@exceptions/not-found-exceptions";
 import { userModel } from "@modules/user/user.model";
 import { getUserByEmail } from "@modules/user/user.service";
+import { validadePassword } from "@utils/password";
 import { AuthDTO } from "./dtos/auth.dto";
 
-export const validateAuth = (authDto: AuthDTO): Promise<userModel> => {
-  const user = getUserByEmail(authDto.email);
+export const validateAuth = async (authDto: AuthDTO): Promise<userModel> => {
+  const user = await getUserByEmail(authDto.email);
+  const isValidPassword = await validadePassword(authDto.password, user.password);
+
+  if (!isValidPassword) {
+    throw new notFoundException("User");
+  }
 
   return user;
 };
