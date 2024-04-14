7de59367144b073e38dc33af411e093ba97719b8
mrosa16 <michael.rosa@holu.com.br>
2024-04-14 18:41:31 -0300

    Feat:  create middleware auth

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,32 +1,13 @@
 import { ReturnError } from "@exceptions/dtos/return-error.dto";
 import { notFoundException } from "@exceptions/not-found-exceptions";
-import { verifyToken } from "@utils/auth";
 import { Request, Response, Router } from "express";
+import { authMiddleware } from "src/middlewares/auth.middleware";
 import { userInsertDTO } from "./dtos/user-insert.dto";
 import { createUser, getUsers } from "./user.service";
 
 const userRouter = Router();
 const router = Router();
-
 userRouter.use("/user", router);
-router.get("/", async (req: Request, res: Response): Promise<void> => {
-  const authorization = req.headers.authorization;
-  verifyToken(authorization)
-    .then(async () => {
-      const users = await getUsers().catch((error) => {
-        if (error instanceof notFoundException) {
-          res.status(204);
-        } else {
-          new ReturnError(res, error);
-        }
-      });
-
-      res.send(users);
-    })
-    .catch((error) => {
-      new ReturnError(res, error);
-    });
-});
 
 router.post(
   "/",
@@ -36,6 +17,28 @@ router.post(
     });
     res.send(user);
   }
+);
+
+router.use(authMiddleware);
+router.get(
+  "/",
+  async (req: Request, res: Response): Promise<void> => {
+    //   const authorization = req.headers.authorization;
+    //   verifyToken(authorization)
+    //     .then(async () => {
+    const users = await getUsers().catch((error) => {
+      if (error instanceof notFoundException) {
+        res.status(204);
+      } else {
+        new ReturnError(res, error);
+      }
+    });
+
+    res.send(users);
+  }
+  // .catch((error) => {
+  //   new ReturnError(res, error);
+  // });
 );
 
 export default userRouter;
